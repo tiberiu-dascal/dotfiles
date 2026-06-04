7da92398cab52058fb9b66c3db06408f91ece242
tiberiu-dascal <tiberiu.purcel@gmail.com>
2026-06-04 20:49:38 +0300

    Optimized zshrc config and cleaned tmux

--- zshrc/.config/zshrc/00-init.zsh
+++ zshrc/.config/zshrc/00-init.zsh
@@ -19,20 +19,20 @@ if command -v emacs &>/dev/null; then
 fi
 
 export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
-[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
-
-# Update Path for GCloud SDK and completion on Mac
-if uname -s | grep -q "Darwin" ;then
-    if [ -f '$HOME/Downloads/google-cloud-sdk/path.zsh.inc' ]; then . '$HOME/Downloads/google-cloud-sdk/path.zsh.inc'; fi
-    if [ -f '$HOME/Downloads/google-cloud-sdk/completion.zsh.inc' ]; then . '$HOME/Downloads/google-cloud-sdk/completion.zsh.inc'; fi
+# Lazy-load nvm: sourcing nvm.sh eagerly costs ~950ms at startup (nvm_auto checks
+# the default node version). Instead, stub the commands and load nvm on first use.
+if [ -s "$NVM_DIR/nvm.sh" ]; then
+  _load_nvm() {
+    unset -f nvm node npm npx _load_nvm 2>/dev/null
+    \. "$NVM_DIR/nvm.sh"
+    [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
+  }
+  nvm()  { _load_nvm; nvm "$@"; }
+  node() { _load_nvm; node "$@"; }
+  npm()  { _load_nvm; npm "$@"; }
+  npx()  { _load_nvm; npx "$@"; }
 fi
 
-if [ "$(hostname)" = "OTX-HBKT2D3" ]; then
-  . "$HOME/.cargo/env"
-  . "$HOME/.local/share/bob/env/env.sh"
-  eval "$(mise activate bash)"
-fi
 
 typeset -A ZSH_HIGHLIGHT_STYLES
 
@@ -46,23 +46,5 @@ FZF_DEFAULT_OPTS="\
   --color=fg:#cdd6f4,header:#f38ba8,info:#cba6f7,pointer:#f5e0dc \
   --color=marker:#f5e0dc,fg+:#cdd6f4,prompt:#cba6f7,hl+:#f38ba8"
 
-# Export TMUX_AUTOSTART=1 on WSL at work (ROTDASCAL01) and on Mac (TBMBPM3.local)
-if [ "$(hostname)" = "OTX-HBKT2D3" ] || [ "$(hostname)" = "tbx12in1" ]; then
-    export TMUX_AUTOSTART=1
-fi
 
-# Start tmux if TMUX_AUTOSTART is set
-if [[ -v TMUX_AUTOSTART ]]; then
-  if [ ! "$TMUX" ]; then
-    tmux attach -t "👨🏻‍💻" || tmux new -s "👨🏻‍💻"
-  fi
-fi
 
-function y() {
-	local tmp="$(mktemp -t "yazi-cwd.XXXXXX")" cwd
-	yazi "$@" --cwd-file="$tmp"
-	if cwd="$(command cat -- "$tmp")" && [ -n "$cwd" ] && [ "$cwd" != "$PWD" ]; then
-		builtin cd -- "$cwd"
-	fi
-	rm -f -- "$tmp"
-}
--- zshrc/.config/zshrc/10-customization.zsh
+++ zshrc/.config/zshrc/10-customization.zsh
@@ -1,17 +1,1 @@
-if [[ hostname == "OTX-HBKT2D3" ]]; then
-    plugins=( git zsh-autosuggestions zsh-syntax-highlighting fast-syntax-highlighting omw )
-else
-    plugins=( git zsh-autosuggestions zsh-syntax-highlighting fast-syntax-highlighting )
-fi
-
-source $ZSH/oh-my-zsh.sh
-source <(fzf --zsh)
-source <(gh completion -s zsh)
-
-HISTFILE=~/.zsh_history
-HISTSIZE=10000
-SAVEHIST=10000
-setopt appendhistory
-
-eval "$(zoxide init zsh)"
-eval "$(starship init zsh)"
+export BAT_THEME="Catppuccin Mocha"
