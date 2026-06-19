6b40ee51a4f0e1f215516860bbfad5aa172d4b29
Tiberiu Dascal <tiberiu.purcel@gmail.com>
2026-06-19 13:54:59 +0300

    Adjusted config for new work laptop

--- zshrc/.config/zshrc/99-work.zsh
+++ zshrc/.config/zshrc/99-work.zsh
@@ -1,13 +1,12 @@
-if [[ $(hostname) == "OTX-HBKT2D3" ]]; then
+if [[ $(hostname) == "OTX-7Y7ZBB4" ]]; then
     PATH=$PATH:$HOME/.local/bin
     PATH=$PATH:/usr/local/go/bin
+    PATH=$PATH:/snap/bin
     M2_HOME=/usr/local/apache-maven/apache-maven-3.8.8
     M2=$M2_HOME/bin
     PATH=$M2:$PATH
     export BAT_THEME="Catppuccin-mocha"
 
-    source "$HOME/.cargo/env"
-    source "$HOME/.local/share/bob/env/env.sh"
     source "$HOME/.sdkman/bin/sdkman-init.sh"
 
     eval "$(mise activate zsh)"
